83b7f3719e35507ec195b04fbac97be0a6323d09
andy8052 <andy8052@protonmail.com>
2021-03-29 20:08:06 -0400

    move from reserve price to reserve total

--- src/test/ERC721TokenVault.t.sol
+++ src/test/ERC721TokenVault.t.sol
@@ -192,38 +192,6 @@ contract VaultTest is DSTest, ERC721Holder {
         curator.call_updateCurator(address(curator));
     }
 
-    function test_updateBasePrice() public {
-        // current reserve price is 1 ETH
-        // lets up base price to 10 ETH. After a transfer new reserve should be 2 ETH (20%)
-        vault.updateBasePrice(10 ether);
-        vault.transfer(address(user1), 50000000000000000000);
-        assertEq(vault.reservePrice(), 2 ether);
-    }
-
-    function test_updateBasePrice2() public {
-        // current reserve price is 1 ETH
-        // lets up base price to 4 ETH. After a transfer new reserve should be 1 ETH
-        vault.updateBasePrice(4 ether);
-        vault.transfer(address(user1), 50000000000000000000);
-        assertEq(vault.reservePrice(), 1 ether);
-    }
-
-    function test_updateBasePrice3() public {
-        // current reserve price is 1 ETH
-        // lets lower base price to 0.1 ETH. After a transfer new reserve should be 0.5 ETH
-        vault.updateBasePrice(0.1 ether);
-        vault.transfer(address(user1), 50000000000000000000);
-        assertEq(vault.reservePrice(), 0.5 ether);
-    }
-
-    function test_updateBasePrice4() public {
-        // current reserve price is 1 ETH
-        // lets lower base price to 0.2 ETH. After a transfer new reserve should be 1 ETH
-        vault.updateBasePrice(0.2 ether);
-        vault.transfer(address(user1), 50000000000000000000);
-        assertEq(vault.reservePrice(), 1 ether);
-    }
-
     function test_updateAuctionLength() public {
         vault.updateAuctionLength(2 weeks);
         assertTrue(vault.auctionLength() == 2 weeks);
@@ -265,42 +233,51 @@ contract VaultTest is DSTest, ERC721Holder {
 
     function test_reservePriceTransfer() public {
         // reserve price here should not change
-        vault.transfer(address(user1), 50000000000000000000);
+        vault.transfer(address(user1), 50e18);
         assertEq(vault.reservePrice(), 1 ether);
+        assertEq(vault.votingTokens(), 50e18);
 
-        assertEq(vault.userPrices(address(user1)), 1 ether);
+        assertEq(vault.userPrices(address(user1)), 0);
 
         // reserve price should update to 1.5 ether
         user1.call_updatePrice(2 ether);
         assertEq(vault.reservePrice(), 1.5 ether);
 
-        // now user 1 sends half their tokens to user 2
-        // reserve price is 1 ether * 0.5 + 2 ether * 0.25 + 1.5 ether * 0.25
-        user1.call_transfer(address(user2), 25000000000000000000);
+        // now user 1 sends 2/5 their tokens to user 2
+        // reserve price is now 1 * 5 + 2 * 3 / 8 = 1.375
+        user1.call_transfer(address(user2), 20e18);
         assertEq(vault.reservePrice(), 1.375 ether);
+
+        // now they are voting the same as user1 was so we go back to 1.5 eth
+        user2.call_updatePrice(2 ether);
+        assertEq(vault.reservePrice(), 1.5 ether);
 
         // send all tokens back to first user
         // their reserve price is 1 ether and they hold all tokens
-        user1.call_transfer(address(this), 25000000000000000000);
-        user2.call_transfer(address(this), 25000000000000000000);
+        user1.call_transfer(address(this), 30e18);
+        user2.call_transfer(address(this), 20e18);
         assertEq(vault.reservePrice(), 1 ether);
     }
 
     function test_bid() public {
-        vault.transfer(address(user1), 25000000000000000000);
-        vault.transfer(address(user2), 25000000000000000000);
-        vault.transfer(address(user3), 50000000000000000000);
+        vault.transfer(address(user1), 25e18);
+        user1.call_updatePrice(1 ether);
+        vault.transfer(address(user2), 25e18);
+        user2.call_updatePrice(1 ether);
+        vault.transfer(address(user3), 50e18);
+        user3.call_updatePrice(1 ether);
 
         user1.call_start(1.05 ether);
 
-        assertTrue(vault.auctionLive());
+        assertTrue(vault.auctionState() == TokenVault.State.live);
 
-        uint256 bal = address(user1).balance;
+        uint256 bal = IWETH(vault.weth()).balanceOf(address(user1));
         user2.call_bid(1.5 ether);
-        assertEq(bal + 1.05 ether, address(user1).balance);
-        bal = address(user2).balance;
+        assertEq(bal + 1.05 ether, IWETH(vault.weth()).balanceOf(address(user1)));
+
+        bal = IWETH(vault.weth()).balanceOf(address(user2));
         user1.call_bid(2 ether);
-        assertEq(bal + 1.5 ether, address(user2).balance);
+        assertEq(bal + 1.5 ether, IWETH(vault.weth()).balanceOf(address(user2)));
 
         hevm.warp(block.timestamp + 7 days);
 
@@ -308,41 +285,48 @@ contract VaultTest is DSTest, ERC721Holder {
 
         assertEq(token.balanceOf(address(user1)), 1);
 
-        // auction has ended. Now lets get all token holders their ETH
+        // auction has ended. Now lets get all token holders their WETH since they are contracts
         // user1 gets 1/4 of 2 ETH or 0.5 ETH
         // user2 gets 1/4 of 2 ETH or 0.5 ETH
         // this gets 1/2 of 2 ETH or 1 ETH
-        uint256 user1Bal = address(user1).balance;
-        uint256 user2Bal = address(user2).balance;
-        uint256 user3Bal = address(user3).balance;
+        uint256 user1Bal = IWETH(vault.weth()).balanceOf(address(user1));
+        uint256 user2Bal = IWETH(vault.weth()).balanceOf(address(user2));
+        uint256 user3Bal = IWETH(vault.weth()).balanceOf(address(user3));
 
         user1.call_cash();
-        assertEq(user1Bal + 0.5 ether, address(user1).balance);
-        user2.call_cash();
-        assertEq(user2Bal + 0.5 ether, address(user2).balance);
-        user3.call_cash();
-        assertEq(user3Bal + 1 ether, address(user3).balance);
+        uint256 wethBal = IWETH(vault.weth()).balanceOf(address(user1));
+        assertEq(user1Bal + 0.5 ether, wethBal);
 
-        assertTrue(!vault.auctionLive());
-        assertTrue(vault.vaultClosed());
+        user2.call_cash();
+        wethBal = IWETH(vault.weth()).balanceOf(address(user2));
+        assertEq(user2Bal + 0.5 ether, wethBal);
+
+        user3.call_cash();
+        wethBal = IWETH(vault.weth()).balanceOf(address(user3));
+        assertEq(user3Bal + 1 ether, wethBal);
+
+        assertTrue(vault.auctionState() == TokenVault.State.ended);
     }
 
     function test_redeem() public {
         vault.redeem();
-        assertTrue(!vault.auctionLive());
-        assertTrue(vault.vaultClosed());
+
+        assertTrue(vault.auctionState() == TokenVault.State.redeemed);
 
         assertEq(token.balanceOf(address(this)), 1);
     }
 
     function test_cantGetEth() public {
         vault.transfer(address(user1), 25000000000000000000);
+        user1.call_updatePrice(1 ether);
         vault.transfer(address(user2), 25000000000000000000);
+        user2.call_updatePrice(1 ether);
         vault.transfer(address(user4), 50000000000000000000);
+        user4.call_updatePrice(1 ether);
 
         user4.call_start(1.05 ether);
         user4.setCanReceive(false);
-        assertTrue(vault.auctionLive());
+        assertTrue(vault.auctionState() == TokenVault.State.live);
 
         user2.call_bid(1.5 ether);
         uint256 wethBal = IWETH(vault.weth()).balanceOf(address(user4));
